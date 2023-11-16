dc2b4f286d566009ec41af8f74c8443fb2cfae95
Inferno214221 <inferno214221@gmail.com>
2023-11-16 17:38:18 +1030

    Handle Images, Await Page Loading

--- index.js
+++ index.js
@@ -43,34 +43,37 @@ await page.waitForSelector("#app-mount > div.appAsidePanelWrapper__714a6 > div.n
 go = true;//FIXME:
 
 class ChannelBridge {
-    constructor(matrix, discord) {
+    constructor(matrix, discordServer, discordChannel, name = null) {
         this.matrixRoom = matrix;
-        this.discordServer = discord.server;
-        this.discordChannel = discord.channel;
-        this.discord = discord.server + "/" + discord.channel;
+        this.discordServer = discordServer;
+        this.discordChannel = discordChannel;
+        this.discord = discordServer + "/" + discordChannel;
+        this.name = name;
         this.lastDiscordMsg = new Date().getTime();
         this.matrixUsername = mxClient.getProfileInfo(DATA.matrix.userId, "displayname");
-        this.initPage();
     }
+
     async initPage() {
         this.page = await browser.newPage();
         
         await this.page.goto("https://discord.com/login/");
         //Login redirects for a browser that has already done it.
         await this.page.setViewport({width: 1080, height: 800});
-        await this.page.waitForSelector("#app-mount > div.appAsidePanelWrapper__714a6 > div.notAppAsidePanel__9d124 > div.app_b1f720 > div > div.layers__1c917.layers_a23c37 > div > div > nav > ul");
+        // await this.page.waitForSelector("#app-mount > div.appAsidePanelWrapper__714a6 > div.notAppAsidePanel__9d124 > div.app_b1f720 > div > div.layers__1c917.layers_a23c37 > div > div > nav > ul");
 
         await this.page.goto("https://discord.com/channels/" + this.discord);
+        // this.page.goto("https://discord.com/channels/" + this.discord);
     }
 
     async getDiscordMessages() {
-        let messages = await this.page.evaluate(() => {
+        let messages = await this.page.evaluate((lastDiscordMsg) => {
+            console.log(lastDiscordMsg);
             let messages = [];
             let element = document.querySelector("#app-mount > div.appAsidePanelWrapper__714a6 > div.notAppAsidePanel__9d124 > div.app_b1f720 > div > div.layers__1c917.layers_a23c37 > div > div > div > div > div.chat__52833 > div.content__1a4fe > div > div.chatContainer__23434 > main > div.messagesWrapper_ea2b0b.group-spacing-16 > div > div > ol").firstElementChild;
             let author;
             let timestamp;
             let message;
-            while (element != undefined) {
+            while (element != undefined) {//TODO: Take timestamp and go backwards
                 if (element.firstElementChild != undefined && element.firstElementChild.firstElementChild != undefined){
                     Array.from(element.firstElementChild.firstElementChild.children).forEach((element) => {
                         Array.from(element.classList).forEach((elementClass) => {
@@ -87,7 +90,8 @@ class ChannelBridge {
                                 case "messageContent__21e69":
                                     // console.log("Message", element);
                                     message = element.innerText;
-                                    messages.push([author, timestamp, message]);
+                                    if (message == "") return;
+                                    messages.push([author, timestamp, message, "m.text"]);
                                     break;
                                 default:
                                     // console.log("None", element);
@@ -95,11 +99,20 @@ class ChannelBridge {
                             }
                         });
                     });
+                    if (element.firstElementChild.firstElementChild.nextElementSibling != null) {
+                        try {
+                            imageUrl = element.firstElementChild.firstElementChild.nextElementSibling.firstElementChild
+                            .firstElementChild.firstElementChild.firstElementChild.firstElementChild.firstElementChild
+                            .firstElementChild.nextElementSibling.firstElementChild.firstElementChild.src;
+                            console.log(imageUrl);
+                            messages.push([author, timestamp, imageUrl, "m.image"]);
+                        } catch {}
+                    }
                 };
                 element = element.nextElementSibling;
             }
             return messages;
-        });
+        }, this.lastDiscordMsg);
         messages.sort((a, b) => new Date(a).getTime() - new Date(b).getTime()).forEach(async (message) => {
             if (message[0] == DATA.discord.username) return;
             if (new Date(message[1]).getTime() > this.lastDiscordMsg) {
@@ -107,10 +120,19 @@ class ChannelBridge {
                     await mxClient.setDisplayName(message[0]);
                     this.matrixUsername = message[0];
                 }
-                await mxClient.sendEvent(this.matrixRoom, "m.room.message", {
-                    msgtype: "m.text",
-                    body: message[2]
-                });
+                
+                switch (message[3]) {
+                    case "m.text":
+                        await mxClient.sendEvent(this.matrixRoom, "m.room.message", {
+                            msgtype: "m.text",
+                            body: message[2]
+                        });
+                        break;
+                    case "m.image":
+                        await mxClient.sendImageMessage(this.matrixRoom, message[2]);
+                        break
+                }
+                
                 this.lastDiscordMsg = new Date(message[1]).getTime();
             }
         });
@@ -130,12 +152,15 @@ class ChannelBridge {
 var pages = [];
 var pageIndexByMatrixRoom = {};
 var pageIndexByDiscord = {};
-Object.keys(BRIDGE).forEach((key) => {
-    let newPage = new ChannelBridge(key, BRIDGE[key]);
+
+console.log("Making Pages: ", BRIDGE);
+for (let key of Object.keys(BRIDGE)) {
+    let newPage = new ChannelBridge(key, BRIDGE[key].server, BRIDGE[key].channel, BRIDGE[key].name);//TODO: Store Timestamps
+    await newPage.initPage();
     let index = pages.push(newPage) - 1;
     pageIndexByMatrixRoom[newPage.matrixRoom] = index;
     pageIndexByDiscord[newPage.discord] = index;
-});
+}
 
 mxClient.on("Room.timeline", function (event, room, toStartOfTimeline) {
     if (!go) {
@@ -146,9 +171,20 @@ mxClient.on("Room.timeline", function (event, room, toStartOfTimeline) {
     }
 });
 
-await new Promise(resolve => setTimeout(resolve, 30000));
+for (let page of pages) {
+    console.log("Waiting for page: " + (page.name || page.discordChannel));
+    //TODO: Handle 'open in desktop app'
+    // try {
+    //     await page.page.waitForSelector("#app-mount > div.appAsidePanelWrapper__714a6 > div.notAppAsidePanel__9d124 > div.app_b1f720 > div > div.layers__1c917.layers_a23c37 > div > div > div > div > div.chat__52833 > div.content__1a4fe > div > div.chatContainer__23434 > main > form > div > div.scrollableContainer__33e06.themedBackground__6b1b6.webkit__8d35a > div > div.textArea__74543.textAreaSlate_e0e383.slateContainer_b692b3 > div > div.markup_a7e664.editor__66464.slateTextArea__0661c.fontSize16Padding__48818",
+    //         { timeout: 3000 });
+    // } catch {}
+    await page.page.waitForSelector("#app-mount > div.appAsidePanelWrapper__714a6 > div.notAppAsidePanel__9d124 > div.app_b1f720 > div > div.layers__1c917.layers_a23c37 > div > div > div > div > div.chat__52833 > div.content__1a4fe > div > div.chatContainer__23434 > main > form > div > div.scrollableContainer__33e06.themedBackground__6b1b6.webkit__8d35a > div > div.textArea__74543.textAreaSlate_e0e383.slateContainer_b692b3 > div > div.markup_a7e664.editor__66464.slateTextArea__0661c.fontSize16Padding__48818",
+        { timeout: 0 });
+}
+console.log("All pages loaded.");
+
+// await new Promise(resolve => setTimeout(resolve, 120000));
 while (true) {
-    console.log("Here");
     pages.forEach((page) => page.getDiscordMessages());
     await new Promise(resolve => setTimeout(resolve, 3000));
 }
